d129d58af724595168338c4d331e0fdffc6cbfe1
luciabenedetta <luciabenedetta.m@icloud.com>
2025-01-15 21:59:18 +0100

    add main.py and notebook.ipynb with initial code examples

---
+++ main.py
@@ -0,0 +1,14 @@
+# text in lower
+word_ON = "ON"
+# name capitalized 
+name = "tom"
+# text in uppercase 
+word_long = "long"
+# replace the a with the o 
+word_walk = "walk"
+# print everithing in one single string 
+text = (word_ON.lower() + " " + name.capitalized() + " " + word_long.uppercase() + " " + word_walk.replace("a", "o"))
+
+print(text)
+
+separation = text.split()
